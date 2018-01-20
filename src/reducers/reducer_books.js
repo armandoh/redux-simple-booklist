bc334f295b2928409f657f89f9f69b031c513937
Armando Hernandez <armando.hdz.b@gmail.com>
2018-01-20 14:20:24 -0600

    Commit the react-redux-booklist project

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Java 8 : Lambdas', pages: 678},
+        { title: 'Hamlet', pages: 1234},
+        { title: 'Parfum', pages: 999},
+        { title: 'Who got my cheese', pages: 345}
+    ]
+}
